cce7ff76fd91fbc1d9ed942e5a6d39c5102d2cf9
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2024-06-25 17:06:30 -0300

    boton de navba, probando admin

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LoginForm from '../LoginForm/LoginForm';
+import { useAuth0 } from '@auth0/auth0-react';
 import './Navbar.css'; // Archivo CSS local
 
 const Navbar = () => {
+  const { user, isAuthenticated } = useAuth0();
+  console.log(user)
+  const adminEmail = 'emanuelgimenez2@gmail.com';
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-brand">
@@ -22,11 +26,13 @@ const Navbar = () => {
               Agendar Turno
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/admin" className="nav-link">
-              Admin
-            </Link>
-          </li>
+           {user.email === adminEmail && ( // Mostrar solo si el correo coincide con el correo de administrador
+            <li className="nav-item">
+              <Link to="/admin" className="nav-link">
+                Admin
+              </Link>
+            </li>
+          )} 
         </ul>
         <LoginForm />
       </div>
